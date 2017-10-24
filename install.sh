58dddbc105a1d1c851ee0f48ea45980e39aa2dfc
Victor Gavro <vgavro@gmail.com>
2017-10-24 18:11:37 +0300

    gtk2/3 font/theme updates

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ ln -vs $PWD/.vimrc ~/.vimrc
 ln -vs $PWD/.gitconfig ~/.gitconfig
 
 # DESKTOP
+ln -vs $PWD/.xinitrc ~/.xinitrc
+ln -vs $PWD/.Xresources ~/.Xresources
+
 mkdir -vp ~/.config/awesome
 ln -vs $PWD/.config/awesome/rc.lua ~/.config/awesome/rc.lua
 ln -vs $PWD/.config/awesome/vgavro-widgets ~/.config/awesome/
@@ -17,10 +20,10 @@ ln -vs $PWD/.config/awesome/vgavro-theme ~/.config/awesome/
 mkdir -vp ~/.config/gtk-3.0
 ln -vs $PWD/.config/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
 
+ln -vs $PWD/.gtkrc-2.0 ~/.gtkrc-2.0
+
 mkdir -vp ~/.config/mpd
 ln -vs $PWD/.config/mpd/mpd.conf ~/.config/mpd/mpd.conf
-ln -vs $PWD/.xinitrc ~/.xinitrc
-ln -vs $PWD/.Xresources ~/.Xresources
 
 mkdir -vp ~/Pictures
 ln -vs $PWD/Pictures/avatar.jpg ~/Pictures/avatar.jpg
