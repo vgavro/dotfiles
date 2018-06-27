ba1c892b20d3e41e0046e43c557cfd93c966da7e
Victor Gavro <vgavro@gmail.com>
2018-06-27 11:26:09 +0200

    centos install improvements

--- install_centos7.sh
+++ install_centos7.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-(( EUID )) && echo ‘You need to be root.’ && exit 1
+set -e
+(( EUID )) && echo 'You need to be root.' && exit 1
 
 yum -y update
 yum -y install yum-utils
 yum -y groupinstall development
-yum -y install vim git wget curl fish htop psmisc
+yum -y install vim git tig wget curl fish htop psmisc
+
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 yum -y install tmux2u python36u python36u-pip python36u-devel
 
-cd /etc/yum.repos.d/
-
-if [ ! -f /etc/yum.repos.d/shells:fish:release:2.repo ]; then
-    wget http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo \
-        -O /etc/yum.repos.d/shells:fish:release:2.repo
-fi
+yum-config-manager --add-repo http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo
 yum -y install fish
