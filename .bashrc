104df6d6c41f9a7e36e18cee80b3861ed697fea2
Victor Gavro <vgavro@gmail.com>
2018-09-16 18:15:21 +0300

    minor changes/improvements

--- .bashrc
+++ .bashrc
@@ -1,11 +1,17 @@
+[[ $- != *i* ]] && return
+# Because of "skeleton" and all this scaffolding shit pattern local .bashrc is not useful anyway
+
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
 
 [ $TERM == rxvt-unicode-256color ] && [ ! -f /usr/share/terminfo/r/rxvt-unicode-256color ] && {
     # apt-get install ncurses-term
-    export TERM=xterm-256color
+    if [ -f /usr/share/terminfo/r/rxvt-256color ]
+        export TERM=rxvt-256color
+    else
+        export TERM=xterm-256color
+    fi
 }
 
-# [[ $- != *i* ]] && return
-# if command -v fish 2>/dev/null; then
-#     exec fish
-# fi
+if command -v fish 2>/dev/null; then
+    exec fish
+fi
--- install.fish
+++ install.fish
@@ -4,6 +4,8 @@ for option in $argv
     switch "$option"
         case -f --force
             set OVERWRITE 1
+        case -d --desktop
+            set DESKTOP 1
         case \*
             printf "error: Unknown option %s\n" $option
     end
@@ -18,28 +20,30 @@ function _map
     ln -vs $SRC/$argv $HOME/$argv
 end
 
-_map .config/fish/config.fish
-
 _map .bashrc
+_map .config/fish/config.fish
 _map .tmux.conf
 _map .vimrc
 _map .gitconfig
 
-# DESKTOP
-# _map .xinitrc
-# _map .Xresources
-#
-# _map .config/awesome/rc.lua
-# _map .config/awesome/vgavro-widgets
-# _map .config/awesome/vgavro-theme
-#
-# _map .config/gtk-3.0/settings.ini
-#
-# _map .gtkrc-2.0
-#
-# _map .config/mpd/mpd.conf
-#
-# _map Pictures/avatar.jpg
-# _map Pictures/face.jpg
+if [ $DESKTOP ]
+    _map .xinitrc
+    _map .Xresources
+
+    _map .config/mimeapps.list
+
+    _map .config/awesome/rc.lua
+    _map .config/awesome/vgavro-widgets
+    _map .config/awesome/vgavro-theme
+
+    _map .config/gtk-3.0/settings.ini
+
+    _map .gtkrc-2.0
+
+    _map .config/mpd/mpd.conf
+
+    _map Pictures/avatar.jpg
+    _map Pictures/face.jpg
+end
 
 functions --erase _map
